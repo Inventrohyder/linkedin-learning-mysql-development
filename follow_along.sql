b39dc246c16ff9d45573b8823288477cfc6e35fd
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-04-01 11:53:45 +0300

    🗃 feat: create movies_basic table

--- follow_along.sql
+++ follow_along.sql
@@ -1,2 +1,14 @@
 -- Create the Database
 CREATE DATABASE movies;
+
+-- Create the movies_basic table
+CREATE TABLE movies.movies_basic (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(100),
+    genre VARCHAR(200),
+    release_year INT,
+    director VARCHAR(40),
+    studio VARCHAR(30),
+    critic_rating DECIMAL(2, 1) DEFAULT 0,
+    PRIMARY KEY(id)
+);
