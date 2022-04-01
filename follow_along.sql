f9397104e3854047ce2c476e42ea8a213b58cbaa
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-04-01 15:53:34 +0300

    🗃 fix: use solutions values

--- follow_along.sql
+++ follow_along.sql
@@ -23,7 +23,7 @@ CHANGE COLUMN director director VARCHAR(50);
 USE movies;
 CREATE TABLE genres (
     id INT AUTO_INCREMENT NOT NULL,
-    genre VARCHAR(200),
+    genre VARCHAR(25),
     PRIMARY KEY(id)
 );
 COMMIT;
@@ -31,14 +31,14 @@ COMMIT;
 CREATE TABLE studios (
     id INT AUTO_INCREMENT NOT NULL,
     studio_name VARCHAR(30),
-    city VARCHAR(30),
+    city VARCHAR(20),
     PRIMARY KEY(id)
 );
 COMMIT;
 
 CREATE TABLE directors (
     id INT AUTO_INCREMENT NOT NUll,
-    dir_name VARCHAR(50),
+    dir_name VARCHAR(42),
     PRIMARY KEY(id)
 );
 COMMIT;
@@ -47,7 +47,7 @@ CREATE TABLE titles (
     id INT AUTO_INCREMENT NOT NULL,
     title  VARCHAR(100),
     genre_id INT,
-    release_year_id INT,
+    release_year_id SMALLINT,
     director_id INT,
     studio_id INT,
     PRIMARY KEY(id),
@@ -61,8 +61,13 @@ CREATE TABLE critic_ratings (
     id INT AUTO_INCREMENT NOT NULL,
     title_id INT,
     critics_rating DECIMAL(2, 1),
-    PRIMARY KEY(id),
-    FOREIGN KEY(title_id) REFERENCES titles(id)
+    PRIMARY KEY(id)
 );
+COMMIT;
+
+ALTER TABLE critic_ratings
+ADD CONSTRAINT title_id_fk
+FOREIGN KEY (title_id)
+REFERENCES titles(id);
 COMMIT;
 -- END CHALLENGE
